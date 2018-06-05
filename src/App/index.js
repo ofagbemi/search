c2043dc6f25e0b207806e2143ad10a3c9a75f737
ofagbemi <jare@robinhood.com>
2018-06-05 13:54:28 -0700

    Initial commit

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Search from '../Search';
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Search />
+    </div>
+  );
+}
---
+++ src/Search/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import './index.css';
+
+// Demo video available at https://cl.ly/3U3f1t0k0q2z
+export default () => <div className="search" />;
